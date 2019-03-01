d9b7d877eff6b859d9be98525ebc2e6654d9d1fe
JJSang <TMSJJSang@163.com>
2019-03-01 16:49:02 +0800

    develop

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,11 +1,12 @@
 import markdown
 
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from comments.forms import CommentForm
+from .models import Post, Category
 
 
 def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
+    post_list = Post.objects.all()
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
@@ -17,12 +18,26 @@ def detail(request, pk):
                                      'markdown.extensions.codehilite',
                                      'markdown.extensions.toc',
                                   ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
 
 
 # 归档
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
                                     created_time__month=month
-                                    ).order_by('-created_time')
+                                    )
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+# 分类
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    # post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    post_list = cate.post_set.all()
     return render(request, 'blog/index.html', context={'post_list': post_list})
